c13abc08ac90c1d58fe6a6f96f3fc7cf5cccaf2e
Andy Dayton <amdayton@gmail.com>
2016-05-14 19:43:40 -0400

    Updated to use static image for now

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ GoogleMaps.KEY = 'AIzaSyCuKjnJWCoUMRLbVFNEkJoFVD0I73u_xJo';
 
 GoogleMaps.load(function(google) {
 
+var img = new Image();
+img.src = 'assets/map.png';
+
+
 var app = createOrbitViewer({
   clearColor: 0x000000,
   clearAlpha: 1.0,
@@ -31,11 +35,15 @@ texture.generateMipmap = false
 // transparent canvas to start (white)
 var canvas = document.createElement('canvas')
 texture.needsUpdate = true
-texture.image = canvas
+texture.image = img
+img.onload = function() {
+  texture.needsUpdate = true;
+}
+
 
 // add a double-sided sphere
 // var geo = new THREE.SphereGeometry(1, 84, 84)
-geo = new THREE.ParametricGeometry( ParametricGeometries.mobius, 40, 40 );
+geo = new THREE.ParametricGeometry( ParametricGeometries.mobius3d, 40, 40 );
 var mat = new THREE.MeshBasicMaterial({
   map: texture,
   side: THREE.DoubleSide
@@ -47,22 +55,22 @@ app.scene.add(mobius)
 // sphere.scale.x = -1
 
 // var location = [78.235865,15.4900308]
-panorama(streetview(), {radius: 1000}, function (err, result) {
-  if (err) throw err
+// panorama(streetview(), {radius: 1000}, function (err, result) {
+//   if (err) throw err
 
-  // load the equirectangular image
-  equirect(result.id, {
-    tiles: result.tiles,
-    canvas: canvas,
-    crossOrigin: 'Anonymous',
-    zoom: getBestZoom()
-  })
-    .on('complete', function (image) {
-      texture.needsUpdate = true
-    })
-    .on('progress', function (ev) {
-      texture.needsUpdate = true
-    })
-})
+//   // load the equirectangular image
+//   equirect(result.id, {
+//     tiles: result.tiles,
+//     canvas: canvas,
+//     crossOrigin: 'Anonymous',
+//     zoom: getBestZoom()
+//   })
+//     .on('complete', function (image) {
+//       texture.needsUpdate = true
+//     })
+//     .on('progress', function (ev) {
+//       texture.needsUpdate = true
+//     })
+// })
 
 });
